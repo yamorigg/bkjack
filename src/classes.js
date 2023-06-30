6a55a52f6f1dda067954e956a1c80d181fd6719c
yamori <near@ymail.ne.jp>
2023-06-30 23:32:52 +0900

    add comment

--- src/classes.js
+++ src/classes.js
@@ -19,9 +19,9 @@ class Deck{
             this.resetDeck();
         }
     }
-
+    //デッキをシャッフルする
     shuffle(){
-
+        // Fisher-Yatesアルゴリズム
         for (let i = this.cards.length - 1; i >= 0; i--){
             let rand = Math.floor(Math.random() * (i + 1));
             let temp = this.cards[i];
@@ -29,27 +29,27 @@ class Deck{
             this.cards[rand] = temp;
         }
     }
-
+    //デッキをリセットする
     resetDeck(){
         this.cards = this.generateDeck();
-        this.shuffle()
-        return this.cards;
-
+        this.shuffle();
     }
 
-    // カードを一枚引く
+    // 先頭からカードを一枚引く
     drawOne(){
+        // カードがない場合はデッキをリセットする
         if (this.cards.length === 0){
             this.resetDeck();
         }
-        return this.cards.pop();
+        return this.cards.shift();
     }
 
-    
+    //デッキを生成する
     generateDeck(){
         let newDeck = [];
         const suits = ['C', 'H', 'S', 'D']
         const rnak = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+        //newDeckにカードを追加
         for (let i = 0; i < suits.length; i++){
             for (let j = 0; j < rnak.length; j++){
                 newDeck.push(new Card(suits[i], rnak[j]))
@@ -70,14 +70,17 @@ String rank : {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
         this.rank = rank;
     }
 
-
+    //カードのスコアを返す
     getRankNumber(){
+        //Aは11として扱う
         if  (this.rank === 'A'){
             return Number(11)
         }
+        //J,Q,Kは10として扱う
         if (this.rank === 'J' || this.rank === 'Q' || this.rank === 'K'){
             return Number(10)
         }
+        //2~10はそのままの値を返す
         return Number(this.rank);
 
     }
